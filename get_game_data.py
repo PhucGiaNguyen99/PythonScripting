f36b518bb6ffe8adf6f4cbca69a4200b072f7875
Phuc <phucgianguyen0199@gmail.com>
2024-11-29 14:59:25 -0500

    Copy files to new folder.

--- get_game_data.py
+++ get_game_data.py
@@ -21,13 +21,42 @@ def find_all_game_paths(source):
     return game_paths
 
 
+def get_name_from_paths(paths, to_strip):
+    new_names = []
+    for path in paths:
+        _, dir_name = os.path.split(path)
+
+        new_dir_name = dir_name.replace(to_strip, "")
+        new_names.append(new_dir_name)
+    return new_names
+
+
+def create_dir(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+
+def copy_and_overwrite(source, dest):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    shutil.copytree(source, dest)
+
+
 def main(source, target):
     cwd = os.getcwd()
     source_path = os.path.join(cwd, source)
     target_path = os.path.join(cwd, target)
 
     game_paths = find_all_game_paths(source_path)
-    print(game_paths)
+    new_game_dirs = get_name_from_paths(game_paths, "_game")
+
+    create_dir(target_path)
+
+    print(new_game_dirs)
+
+    for src, dest in zip(game_paths, new_game_dirs):
+        dest_path = os.path.join(target_path, dest)
+        copy_and_overwrite(src, dest_path)
 
 
 if __name__ == "__main__":
